ab27c5c0f6964f686e7d8874f3c38c923a32e733
Evgeniy Viniychuk <evgeniy@youshido.com>
2021-03-03 15:08:45 +0200

    fixed restaurants list SSR rendering

--- src/pages/home/home-page-enhanced.js
+++ src/pages/home/home-page-enhanced.js
@@ -5,8 +5,8 @@ import RestaurantsListApi from '../../logic/restaurants-list/api/restaurants-lis
 
 export const HomePageEnhanced = translate('pages.home')(HomePage);
 
-HomePageEnhanced.loadUniversalData = async function({ store, router, network }) {};
-
-HomePageEnhanced.loadClientOnlyData = async function({ store, router, network }) {
+HomePageEnhanced.loadUniversalData = async function({ store, router, network }) {
   await store.callSaga(RestaurantsListApi.fetchUniversalData);
 };
+
+HomePageEnhanced.loadClientOnlyData = async function({ store, router, network }) {};
